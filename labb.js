821001d6c932cf54bec19855e85676ed0485be80
adiphussain2001 <46708024+adiphussain2001@users.noreply.github.com>
2019-09-13 14:36:42 +0200

    Delete labb.js

--- labb.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-var args = process.argv.slice.(2); /* Nu kommer det komma en array av alla argument efter defaulterna */
-
-var fs = require('fs');
-var argv = require('minimist')(process.argv.slice(2));
-var file = fs.readFileSync('data/' + argv.file, 'utf8'); 
